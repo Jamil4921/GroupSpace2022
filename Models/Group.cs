695b07b30549f672ec5ec7b4f1aec86a87a42f39
furqan <furqan.jamil.chaud-ry@student.ehb.be>
2022-11-23 11:28:58 +0100

    Add project files.

---
+++ Models/Group.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GroupSpace2022.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+
+        [Display (Name = "Naam")]
+        public string Name { get; set; }
+
+        [Display(Name = "Omschrijving")]
+        public string Description { get; set; }
+
+        [Display(Name = "Gestart")]
+        [DataType (DataType.Date)]
+        public DateTime Started { get; set; }
+
+        [Display(Name = "Beindigd")]
+        [DataType(DataType.Date)]
+
+        public DateTime Ended { get; set; }
+    }
+}
